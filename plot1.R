accdd7562dd17c968a75dc9960e2b46def7d7c03
Gheorghe Muresan <gheorghe@glassdoor.com>
2016-05-15 18:02:57 -0700

    Added date filter for plot 1.

--- plot1.R
+++ plot1.R
@@ -13,22 +13,30 @@ str(powerData)
 summary(powerData)
 names(powerData)
 
+# Process the data - change to DateTime, filter the dates of interest,
+# select just the columns needed for the plot
+library(dplyr)
+library(lubridate)
+
+gap <- mutate(powerData, DateTime = dmy_hms(paste(Date, " ", Time), tz="America/Los_Angeles")) %>%
+    filter(DateTime >= "2007-02-01" & DateTime < "2007-02-03") %>%
+    select(DateTime, Global_active_power)
+str(gap)
+
 # Alternative 1 - build plot, then copy into file
 # Disadvantage: the copy may not be accurate
-hist(powerData$Global_active_power,
+hist(gap$Global_active_power,
      col="red", bg="transparent",
      main="Global Active Power",
-     xlab="Global Active Power", ylab="Frequency",
-     xlim=c(0,6))
+     xlab="Global Active Power (kilowatts)", ylab="Frequency")
 dev.copy(png, file="plot1.1.png", width=480, height=480)
 dev.off()
 
 # Alternative 2 - create the appropriate graphics file device, then build the plot
 png("plot1.png", width=480, height=480)
-hist(powerData$Global_active_power,
+hist(gap$Global_active_power,
      col="red", bg="transparent",
      main="Global Active Power",
-     xlab="Global Active Power", ylab="Frequency",
-     xlim=c(0,6))
+     xlab="Global Active Power (kilowatts)", ylab="Frequency")
 dev.off()
 
